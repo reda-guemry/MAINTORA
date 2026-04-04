a8002f66698811101b8c356e120eb61c8dc293fa
reda-guemry <guemryreda@gmail.com>
2026-04-04 11:19:23 +0000

    creaete the controller

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function login(LoginRequest $request)
+    {
+        
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+/********** API Routes **********/
+
+
+
+
+
 
 
