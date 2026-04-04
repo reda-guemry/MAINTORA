20cefe7a57a82cf171348ac2215d06289b5379e6
reda-guemry <guemryreda@gmail.com>
2026-04-04 19:38:15 +0000

    feat: add AdminMiddleware for role-based access control and update routing for user management

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,27 @@ Route::post('/refresh', [AuthController::class, 'refresh']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
 
+/**
+ *  Protected routes
+ */
+
+Route::middleware(['auth:api'])->group(function () {
+
+    /*
+     * Admin routes
+     */
+    Route::prefix('admin')->middleware(['admin'])->group(function () {
+
+        /*
+         * Manage users Routes
+         */
+        Route::apiResource('users', UserController::class);
+
+
+    });
+
+
+});
 
 
 
