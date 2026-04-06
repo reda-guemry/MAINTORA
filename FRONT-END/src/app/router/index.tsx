72bf7aeb14c04c7f4199c24de9f68526bea61b53
reda-guemry <guemryreda@gmail.com>
2026-04-06 11:36:55 +0000

    feat: add routing with react-router-dom and restructure app components

---
+++ FRONT-END/src/app/router/index.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Home</h1>
+                
+    }
+])
