bfbf37dc33bf8d882e2a253b3683e414f01bfeb8
reda-guemry <guemryreda@gmail.com>
2026-04-27 05:48:42 +0000

    feat: add types for RepairPurchaseOrder and update ClientRepairRequest to include purchase_order

--- FRONT-END/src/features/client-repair-request/index.ts
+++ FRONT-END/src/features/client-repair-request/index.ts
@@ -1,3 +1,6 @@
 export * from "./types/repairRequest";
 export * from "./types/repairRequestResponses";
+export * from "./types/uploadPurchaseOrderResponses";
 export * from "./hooks/usePaginateClientRepairRequests";
+export * from "./hooks/useUploadRepairPurchaseOrder";
+export * from "./components/UploadPurchaseOrderModal";
